a17add212870fd804e0f4e990e6b2367e8fb057f
Nen0-pro <60430096+Nen0-pro@users.noreply.github.com>
2020-02-10 12:17:40 +0100

    Add files via upload

--- HomeworkJavaAd4/script/script.js
+++ HomeworkJavaAd4/script/script.js
@@ -2,22 +2,24 @@ fetch('https://raw.githubusercontent.com/sedc-codecademy/sedc7-04-ajs/master/g2/
     .then(res => res.json())
     .then(students => {
         // console.log(loging(students))
-        // console.log(allMaleStudents(students))
-        // console.log(adultFemales(students))
-        console.log(averageAge(students))
-        // console.log(femLnameEndWithA(students))
+        console.table(allMaleStudents(students));
+        // console.log(adultFemales(students));
+        console.log(averageAge(students));
+        // console.log(femLnameEndWithA(students));
     })
     .catch(err => console.error(err))
 
 
-let allMaleStudents = students => students.filter(s => s.gender === 'Male' && s.city === 'Skopje')
+let allMaleStudents = students => students.filter(s => s.gender === 'Male' && s.city !== 'Skopje');
 
-let adultFemales = students => students.filter(s => s.gender === 'Female' && s.age > 18)
+let adultFemales = students => students.filter(s => s.gender === 'Female' && s.age > 18);
 
-let averageAge = students => students.map(s => s.age).reduce((sum, avgAge)  => sum + avgAge / students.length, 0)
+let averageAge = students => students.map(s => s.age).reduce((sum, avgAge,)  => sum + avgAge , 0) / students.length;
 
-let femLnameEndWithA = students => students.filter(s => s.lastName.endsWith('a'))
+let femLnameEndWithA = students => students.filter(s => s.lastName.endsWith('a') && s.gender === 'Female');
 
 
-let loging = students => students.filter(s => s.averageGrade).map(s => `${s.firstName} ${s.lastName}is comming from ${s.city}, he is ${s.age} years old and he is ${s.averageGrade === 1 ? 'FAILED' : 'PASSED'}  the class`)
+let loging =  students => students.forEach(s => {
+    console.log(`${s.firstName} ${s.lastName}is comming from ${s.city}, he is ${s.age} years old and he is ${s.averageGrade === 1 ? 'FAILED' : 'PASSED'}  the class`)
+});
 
